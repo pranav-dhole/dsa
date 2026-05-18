f9981c8bc958217c6d224df2ee3d84384a1122c5
Bizzare <bizzareforyou@gmail.com>
2026-05-18 21:21:41 +0530

    MyQueue that uses two stacks behind to fullfill its operations

--- index.js
+++ index.js
@@ -1280,7 +1280,7 @@ MyStack.prototype.top = function () {
     this.q2.push(this.q1.shift());
   }
 
-  let front = this.q1[0]; // this is our top element
+  let front = this.q1.shift(); // this is our top element
 
   // inserting the top element in q1 because we didnt push it in q2 in above loop
   this.q2.push(front);
@@ -1332,4 +1332,49 @@ MyStack2.prototype.top = function () {
 
 MyStack2.prototype.empty = function () {
   return this.q.length === 0;
+};
+
+// write a function that implements a queue using two stacks
+var MyQueue = function () {
+  this.s1 = [];
+  this.s2 = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.s1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  if (this.s2.length === 0) {
+    while (this.s1.length) {
+      this.s2.push(this.s1.pop());
+    }
+  }
+  return this.s2.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (this.s2.length === 0) {
+    while (this.s1.length) {
+      this.s2.push(this.s1.pop());
+    }
+  }
+  return this.s2[this.s2.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return this.s1.length === 0 && this.s2.length === 0;
 };
