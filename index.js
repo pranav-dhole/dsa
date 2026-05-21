2bc1b06c9d0f1ddae75439d5424fbd9e2244a207
Bizzare <bizzareforyou@gmail.com>
2026-05-21 12:17:25 +0530

    removeOuterParentheses() that returns output having no outer parentheses

--- index.js
+++ index.js
@@ -1466,3 +1466,28 @@ MinStack.prototype.getMin = function () {
 
 // Output
 // [null,null,null,null,-3,null,0,-2]
+
+// write a function that returns a string by removing its outermost valid parentheses brackets.
+// 1st approach using stack
+function removeOuterParentheses(s) {
+  let stack = [];
+  let ans = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(s[i]);
+      if (stack.length > 1) {
+        ans += s[i];
+      }
+    } else {
+      if (stack.length > 1) {
+        ans += s[i];
+      }
+      stack.pop();
+    }
+  }
+
+  return ans;
+}
+
+console.log(removeOuterParentheses("(()())(())(()(()))")); //()()()()(())
