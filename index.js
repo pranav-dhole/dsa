d0dce524d599a4116c17e7097f6ab4f937a713e1
Bizzare <bizzareforyou@gmail.com>
2026-05-31 21:09:17 +0530

    findMin(nums) that returns minimum integer using log n time complexity

--- index.js
+++ index.js
@@ -1829,5 +1829,26 @@ function findPeakElement(nums) {
   return l;
 }
 
-console.log(findPeakElement([1, 2, 3, 1])); //returns 2
-console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4])); //returns 5
+// console.log(findPeakElement([1, 2, 3, 1])); //returns 2
+// console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4])); //returns 5
+
+// write a function that returns the minimum value in an rotated sorted array
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let middle = left + Math.floor((right - left) / 2);
+    if (nums[right] < nums[middle]) {
+      left = middle + 1;
+    } else {
+      right = middle;
+    }
+  }
+
+  return nums[left];
+}
+
+console.log(findMin([3, 4, 5, 1, 2])); // returns 1
+console.log(findMin([4, 5, 6, 7, 0, 1, 2])); // returns 0
+console.log(findMin([11, 13, 15, 17])); // returns 11
